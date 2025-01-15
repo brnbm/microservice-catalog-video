fac1cec418085c468962cf1690f9499ed31af9d2
Bruno Barros <brunobm@live.com>
2025-01-15 12:16:48 -0300

    :sparkles: feat: Implementando PaginationPresenter.

--- app/Presenters/PaginationPresenter.php
+++ app/Presenters/PaginationPresenter.php
@@ -3,48 +3,54 @@
 namespace App\Presenters;
 
 use Core\Domain\Repository\PaginationInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
+use stdClass;
 
 class PaginationPresenter implements PaginationInterface
 {
-    public function __construct(object $data) {}
+    public function __construct(private LengthAwarePaginator $paginator) {}
 
+    /**
+     * @return stdClass[]
+     */
     public function items(): array
     {
-        return [];
+        return array_map(
+            function ($item) {
+                $stdClass = new stdClass();
+
+                foreach ($item as $key => $value) {
+                    $stdClass->$key = $value;
+                }
+
+                return $stdClass;
+            },
+            $this->paginator->items()
+        );
     }
 
     public function total(): int
     {
-        return 0;
+        return $this->paginator->total();
     }
 
     public function lastPage(): int
     {
-        return 0;
+        return $this->paginator->lastPage();
     }
 
     public function firstPage(): int
     {
-        return 0;
+        return $this->paginator->firstItem();
     }
 
     public function currentPage(): int
     {
-        return 0;
+        return $this->paginator->currentPage();
     }
 
     public function perPage(): int
     {
-        return 0;
-    }
-
-    public function to(): int
-    {
-        return 0;
-    }
-
-    public function from(): int
-    {
-        return 0;
+        return $this->paginator->perPage();
     }
 }
