6f9ac7fa05a582517e46fec7899777d7bd13ba29
Bruno Barros <brunobm@live.com>
2025-02-09 13:22:08 -0300

    :test_tube: test: Teste de caso de uso de listagem de Categorias.

---
+++ tests/Feature/Core/UseCase/Category/ListCategoriesUseCaseTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use Tests\TestCase;
+use App\Repositories\CategoryRepository;
+use App\Models\Category as CategoryModel;
+use Core\UseCase\Category\ListCategoriesUseCase;
+use Core\UseCase\DTO\Category\ListCategoriesInputDTO;
+
+class ListCategoriesUseCaseTest extends TestCase
+{
+    private $useCase;
+
+    public function testListEmpty(): void
+    {
+        $responseUseCase = $this->useCase->execute(new ListCategoriesInputDTO());
+        $this->assertCount(0, $responseUseCase->items);
+    }
+
+    public function testListAll(): void
+    {
+        CategoryModel::factory()->count(20)->create();
+
+        $responseUseCase = $this->useCase->execute(new ListCategoriesInputDTO());
+        $this->assertEquals(20, $responseUseCase->total);
+    }
+
+    protected function setUp(): void
+    {
+        $repository = new CategoryRepository(new CategoryModel());
+        $this->useCase = new ListCategoriesUseCase($repository);
+
+        parent::setUp();
+    }
+}
