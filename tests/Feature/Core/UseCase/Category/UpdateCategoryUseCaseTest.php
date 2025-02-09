57757a022782ee29b03a966f035555df2140edb7
Bruno Barros <brunobm@live.com>
2025-02-09 16:29:21 -0300

    :test_tube: test: Teste de caso de uso de atualização de Categoria.

---
+++ tests/Feature/Core/UseCase/Category/UpdateCategoryUseCaseTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use Tests\TestCase;
+use App\Repositories\CategoryRepository;
+use App\Models\Category as CategoryModel;
+use Core\UseCase\Category\UpdateCategoryUseCase;
+use Core\UseCase\DTO\Category\CategoryUpdateInputDTO;
+
+class UpdateCategoryUseCaseTest extends TestCase
+{
+
+    public function testUpdate(): void
+    {
+        $repository = new CategoryRepository(new CategoryModel());
+        $useCase = new UpdateCategoryUseCase($repository);
+
+        $categoryDb = CategoryModel::factory()->create();
+
+        $updateInputDto = new CategoryUpdateInputDTO(
+            id: $categoryDb->id,
+            name: 'Category 1 Updated',
+            description: 'Category 1 Description Updated'
+        );
+
+        $resposeUseCase = $useCase->execute($updateInputDto);
+
+        $this->assertEquals($updateInputDto->name, $resposeUseCase->name);
+    }
+}
