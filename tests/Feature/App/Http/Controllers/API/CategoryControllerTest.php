8df874981a3e5aaa1920e4d540e678d65cb5276f
Bruno Barros <brunobm@live.com>
2025-05-24 15:48:44 -0300

    :test_tube: test: Teste de integração da CategoryController - store().

--- tests/Feature/App/Http/Controllers/API/CategoryControllerTest.php
+++ tests/Feature/App/Http/Controllers/API/CategoryControllerTest.php
@@ -4,31 +4,59 @@ namespace Tests\Feature\App\Http\Controllers\API;
 
 use Tests\TestCase;
 use App\Models\Category;
-use Illuminate\Http\Request;
 use PHPUnit\Framework\Attributes\Test;
 use App\Repositories\CategoryRepository;
-use Core\UseCase\Category\ListCategoriesUseCase;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\{
+    Request,
+    JsonResponse
+};
+use Core\UseCase\Category\{
+    CreateCategoryUseCase,
+    ListCategoriesUseCase
+};
+use Symfony\Component\HttpFoundation\{
+    ParameterBag,
+    Response
+};
 
 class CategoryControllerTest extends TestCase
 {
+    protected $controller;
     protected $repository;
 
     #[Test]
     public function index(): void
     {
         $listCategoriesUseCase = new ListCategoriesUseCase($this->repository);
-
-        $controler = new CategoryController();
-        $response = $controler->index(new Request(), $listCategoriesUseCase);
+        $response = $this->controller->index(new Request(), $listCategoriesUseCase);
 
         $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
         $this->assertArrayHasKey('meta', $response->additional);
     }
 
+    #[Test]
+    public function store(): void
+    {
+        $request = new StoreCategoryRequest();
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(new ParameterBag([
+            'name' => 'Test Category',
+            'description' => 'Test Description',
+        ]));
+
+        $createCategoryUseCase = new CreateCategoryUseCase($this->repository);
+        $response = $this->controller->store($request, $createCategoryUseCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+    }
+
     protected function setUp(): void
     {
+        $this->controller = new CategoryController();
         $this->repository = new CategoryRepository(new Category());
         parent::setUp();
     }
