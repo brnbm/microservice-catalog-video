d1171bf95c20934a8a503b703acbf8030c0f618a
Bruno Barros <brunobm@live.com>
2025-05-29 08:36:41 -0300

    :wrench: chore: Registrando o bind de ExceptionHandler.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,19 @@
 <?php
 
+use App\Exceptions\Handler;
 use Illuminate\Foundation\Application;
 use App\Repositories\CategoryRepository;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\{
+    Exceptions,
+    Middleware
+};
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Core\Domain\Repository\CategoryRepositoryInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withSingletons([
         CategoryRepositoryInterface::class => CategoryRepository::class,
+        ExceptionHandler::class => Handler::class,
     ])
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
