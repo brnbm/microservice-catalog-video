098b4b4f0700a9a18d7103c0c9fdc6d8bdf1d04e
Bruno Barros <brunobm@live.com>
2025-06-12 08:19:40 -0300

    :recycle: refactor: Mundando o local do registro da injeção de dependência.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use App\Exceptions\Handler;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use App\Repositories\Transaction\DBTransaction;
+use Core\UseCase\Interfaces\TransactionInterface;
+use App\Repositories\Eloquent\CategoryRepository;
+use Core\Domain\Repository\CategoryRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +17,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->singleton(ExceptionHandler::class, Handler::class);
+
+        $this->app->bind(TransactionInterface::class, DBTransaction::class);
     }
 
     /**
